612916a3de5304b73955f7f68f822ebdc53b2cef
Фёдор Лодиков <fedor_ramoz@mail.ru>
2025-07-26 10:10:33 +0300

    chore(config): вынес конфиг TypeORM в отдельный файл и добавил synchronize в env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import typeOrmConfig from '../config/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
-        entities: [__dirname + '/**/entities/*.entity.{ts,js}'],
-        synchronize: true,
-      }),
-    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
   ],
   controllers: [AppController],
   providers: [AppService],
