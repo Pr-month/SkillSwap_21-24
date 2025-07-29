55633059d8868d55e6caa92f06d41d2736fc2df4
Roland <machodin@yandex.ru>
2025-07-29 19:12:43 +0300

    fix(config) проблема подключения после переноса в папку backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { AppConfigType } from './config/config.type';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const config = configService.get<AppConfigType>('APP_CONFIG')!;
-  await app.listen(config.port);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
