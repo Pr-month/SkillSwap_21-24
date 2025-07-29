65231df3edc4db614e1f22dc839eaf5f91e39eea
Фёдор Лодиков <fedor_ramoz@mail.ru>
2025-07-29 09:52:01 +0300

    feat(config): централизованный и типизированный конфиг приложения и БД через registerAs

---
+++ backend/config/configuration.ts
@@ -0,0 +1,8 @@
+import { registerAs } from '@nestjs/config';
+import typeOrmConfig from './typeorm.config';
+
+export const configuration = registerAs('APP_CONFIG', () => ({
+  db: typeOrmConfig,
+  // port: process.env.PORT || 3000,
+  // jwtSecret: process.env.JWT_SECRET,
+}));
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import typeOrmConfig from '../config/typeorm.config';
+import { configuration } from '../config/configuration';
+import { AppConfigType } from '../config/config.type';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [configuration.KEY],
+      useFactory: (config: AppConfigType) => ({
+        ...config.db,
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
