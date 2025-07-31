08063728e72955e26b02d442f710abfe01acb0ac
Фёдор Лодиков <fedor_ramoz@mail.ru>
2025-07-31 21:15:15 +0300

    feat(core): подключена глобальная ValidationPipe для валидации входящих данных

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AppConfigType } from '../config/config.type';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const config = configService.get<AppConfigType>('APP_CONFIG')!;
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(config.port);
 }
 void bootstrap();
