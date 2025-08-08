a186d1ed57443879648cf7cce4ba56da5e4e3e1c
Alex_V <venedykhin@yandex.ru>
2025-08-08 12:19:18 +1200

    feat(categories.service.ts): добавлен CategoriesService для работы с категориями

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, IsNull } from 'typeorm';
+import { CategoryEntity } from './entities/categories.entity';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private categoriesRepository: Repository<CategoryEntity>,
+  ) {}
+
+  async findAll(): Promise<CategoryEntity[]> {
+    return await this.categoriesRepository.find({
+      where: { parent: IsNull() },
+      relations: ['children'],
+    });
+  }
+
+  async findOne(id: number): Promise<CategoryEntity> {
+    const category = await this.categoriesRepository.findOne({
+      where: { id: id },
+      relations: ['children', 'parent'],
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category with ID ${id} not found`);
+    }
+
+    return category;
+  }
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<CategoryEntity> {
+    const category = this.categoriesRepository.create(createCategoryDto);
+    return await this.categoriesRepository.save(category);
+  }
+
+  async update(
+    id: number,
+    updateCategoryDto: UpdateCategoryDto,
+  ): Promise<CategoryEntity> {
+    await this.findOne(id);
+    await this.categoriesRepository.update(id, updateCategoryDto);
+    return await this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    const category = await this.findOne(id);
+    await this.categoriesRepository.remove(category);
+  }
+}
