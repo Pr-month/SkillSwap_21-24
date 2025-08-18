14cacbeaa41702f9b0377b88c2696c99b94aeb85
Ilya Selenzev <blizard_sis@rambler.ru>
2025-08-18 09:31:13 +0300

    fix(skills) вернул форматирование DTO

--- backend/src/skills/dto/skill.dto.ts
+++ backend/src/skills/dto/skill.dto.ts
@@ -16,7 +16,16 @@ export class CreateSkillDTO {
 }
 
 export class PaginationQueryDto {
-  @IsOptional() @Type(() => Number) @IsInt() @Min(1) page: number = 1;
-  @IsOptional() @Type(() => Number) @IsInt() @Min(1) @Max(100) limit: number =
-    20;
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  limit: number = 20;
 }
