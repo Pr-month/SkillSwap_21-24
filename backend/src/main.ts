a465de9384cf10a08aac11be2d5112c67b3c1a2f
Матвей Хитров <Khitrovmatvei@yandex.ru>
2025-08-21 00:50:29 +0600

    fix setup swagger in main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,8 +6,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { AppConfigType } from './config/config.type';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-//для свагера
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig, swaggerOptions } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -24,53 +24,8 @@ async function bootstrap() {
   );
 
   // Swagger configuration
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('API')
-    .setDescription('API documentation')
-    .setVersion('1.0')
-    .addTag('auth', 'Authentication endpoints')
-    .addTag('categories', 'Categories management endpoints')
-    .addTag('files', 'Files endpoints')
-    .addTag('requests', 'Request management endpoints')
-    .addTag('skills', 'Skills management endpoints')
-    .addTag('users', 'User management endpoints')
-
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'access-token',
-    )
-    .addCookieAuth('refreshToken', {
-      type: 'apiKey',
-      in: 'cookie',
-      name: 'refreshToken',
-      description: 'Refresh token for authentication',
-    })
-    .build();
-
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api/docs', app, document, {
-    swaggerOptions: {
-      persistAuthorization: true,
-      tagsSorter: 'alpha',
-      operationsSorter: 'alpha',
-      docExpansion: 'none',
-      filter: true,
-    },
-    customSiteTitle: 'Mentor Match API Documentation',
-    customCss: `
-      .swagger-ui .topbar { display: none; }
-      .swagger-ui .info { margin: 20px 0; }
-    `,
-  });
-
-  // -----------------------
+  SwaggerModule.setup('api/docs', app, document, swaggerOptions);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   const logger: LoggerService = app.get(WINSTON_MODULE_NEST_PROVIDER);
