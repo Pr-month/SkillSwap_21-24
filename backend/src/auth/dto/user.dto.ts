5d68e8ef24231f05b2cdd73dfd082d1ee41afe60
Ilya Selenzev <blizard_sis@rambler.ru>
2025-08-24 09:21:29 +0300

    fix(user,auth): исправление DTO, убрал дублирование

--- backend/src/auth/dto/user.dto.ts
+++ backend/src/auth/dto/user.dto.ts
@@ -3,34 +3,18 @@ import { IsEmail, IsEnum, IsNumber, IsString, IsUrl } from 'class-validator';
 import { Gender } from 'src/users/enums';
 
 export class CreateUserDTO {
-  @IsString()
-  name: string;
-  @IsEmail()
-  email: string;
-  @IsString()
-  password: string;
-  @IsString()
-  about: string;
-  @IsString()
-  birthdate: string;
-  @IsString()
-  city: string;
-  @IsEnum(Gender)
-  gender: Gender;
-  @IsUrl()
-  avatar: string;
-  @IsNumber()
-  category: number;
-  // @IsString()
-  // subcategory: string;
-  // @ValidateNested({ each: true })
-  // @Type(() => CreateSkillDTO)
-  // skill: CreateSkillDTO;
+  @IsString() name: string;
+  @IsEmail() email: string;
+  @IsString() password: string;
+  @IsString() about: string;
+  @IsString() birthdate: string;
+  @IsString() city: string;
+  @IsEnum(Gender) gender: Gender;
+  @IsUrl() avatar: string;
+  @IsNumber() category: number;
 }
 
 export class LoginUserDTO {
-  @IsEmail()
-  email: string;
-  @IsString()
-  password: string;
+  @IsEmail() email: string;
+  @IsString() password: string;
 }
--- backend/src/users/dto/user.dto.ts
+++ backend/src/users/dto/user.dto.ts
@@ -1,21 +1,8 @@
 import { Exclude, Expose } from 'class-transformer';
-import { IsEmail, IsEnum, IsString, IsUrl } from 'class-validator';
 
 import { SkillEntity } from '../../skills/entities/skills.entity';
 
 import { Gender, UserRole } from '../enums';
-
-export class CreateUserDTO {
-  @IsString() name: string;
-  @IsEmail() email: string;
-  @IsEnum(UserRole) role: UserRole;
-  @IsString() password: string;
-  @IsString() about: string;
-  @IsString() birthdate: string;
-  @IsString() city: string;
-  @IsEnum(Gender) gender: Gender;
-  @IsUrl() avatar: string;
-}
 
 @Exclude()
 export class ResponceUserDTO {
