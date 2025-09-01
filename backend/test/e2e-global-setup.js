418ef05336ba017f694872f514fe78d97fce0103
Ilya Selenzev <blizard_sis@rambler.ru>
2025-09-01 15:17:11 +0300

    fix(test): добавил совместимость в windows для e2e

--- backend/test/e2e-global-setup.js
+++ backend/test/e2e-global-setup.js
@@ -1,10 +1,13 @@
 const { PostgreSqlContainer } = require('@testcontainers/postgresql');
 const { execSync } = require('child_process');
+
 const fs = require('fs');
 const path = require('path');
 
+require('dotenv').config({ path: path.join(process.cwd(), '.env.test') });
+
 module.exports = async () => {
-  const DB_NAME = process.env.POSTGRES_DB || 'SkillSwapDB_test';
+  const DB_NAME = process.env.POSTGRES_DB || 'test';
   const DB_USER = process.env.POSTGRES_USER || 'admin';
   const DB_PASS = process.env.POSTGRES_PASSWORD || 'admin';
   const container = await new PostgreSqlContainer('postgres:17')
@@ -13,6 +16,8 @@ module.exports = async () => {
     .withPassword(DB_PASS)
     .start();
 
+
+  console.log(DB_NAME, DB_PASS, DB_USER);
   process.env.NODE_ENV = 'test';
   process.env.DB_HOST = container.getHost();
   process.env.DB_PORT = String(container.getMappedPort(5432));
