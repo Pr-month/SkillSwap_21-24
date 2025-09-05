b586ac68c5832f5bccfd0c5cda9a281e4609cc92
Alex_V <venedykhin@yandex.ru>
2025-09-05 12:35:55 +1200

    feat(category-list-response.dto.ts): Добавлен CategoryListResponseDto, который определяет структуру ответа для ответа GET /categories

---
+++ backend/src/categories/dto/category-list-response.dto.ts
@@ -0,0 +1,16 @@
+// src/categories/dto/category-list-response.dto.ts
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CategoryListResponseDto {
+  @ApiProperty({ description: 'Уникальный идентификатор категории' })
+  id: number;
+
+  @ApiProperty({ description: 'Название категории' })
+  name: string;
+
+  @ApiProperty({
+    description: 'Дочерние категории',
+    type: () => [CategoryListResponseDto],
+  })
+  children: CategoryListResponseDto[];
+}
