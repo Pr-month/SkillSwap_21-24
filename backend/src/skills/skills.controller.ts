14e5e80f785cc910ba1a2b6c3e9382375c11bba5
Ilya Selenzev <blizard_sis@rambler.ru>
2025-09-12 16:14:53 +0300

    feat(swagger): add swagger documentation to skills

--- backend/src/skills/skills.controller.ts
+++ backend/src/skills/skills.controller.ts
@@ -18,22 +18,38 @@ import {
 
 import { ReqWithUser } from '../auth/auth.types';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-//import { RolesGuard } from '../auth/guards/roles.guard';
-//import { Roles } from '../auth/decorators/roles.decorator';
-//import { UserRole } from '../users/enums';
 
 import { SkillsService } from './skills.service';
 import { SkillEntity } from './entities/skills.entity';
-import { CreateSkillDTO, PaginationQueryDto } from './dto/skill.dto';
+import {
+  CreateSkillDTO,
+  UpdateSkillDTO,
+  SkillResponseDto,
+  PaginationQueryDto,
+} from './dto/skill.dto';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 export type Paginated<T> = { data: T[]; page: number; totalPages: number };
-
+@ApiTags('skills')
 @Controller('skills')
 export class SkillsController {
   constructor(private readonly skillsService: SkillsService) {}
 
   // Получение всех навыков
   @Get()
+  @ApiOperation({ summary: 'Список скиллов (с пагинацией)' })
+  @ApiOkResponse({
+    type: [SkillResponseDto],
+    description: 'Получение всех скиллов',
+  })
   @UsePipes(new ValidationPipe({ transform: true }))
   @HttpCode(HttpStatus.OK)
   async getAllSkills(
@@ -44,6 +60,9 @@ export class SkillsController {
 
   // Создание нового навыка
   @Post()
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Создать скилл' })
+  @ApiCreatedResponse({ type: SkillResponseDto, description: 'Скилл создан' })
   @UseGuards(JwtAuthGuard)
   @HttpCode(HttpStatus.CREATED)
   async createSkill(
@@ -56,6 +75,10 @@ export class SkillsController {
   // Изменение навыка
   @Patch(':id')
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Обновить скилл' })
+  @ApiBody({ type: UpdateSkillDTO })
+  @ApiOkResponse({ type: SkillResponseDto, description: 'Скилл обновлен' })
   @HttpCode(HttpStatus.OK)
   async updateSkill(
     @Param('id', ParseIntPipe) id: number,
@@ -68,6 +91,9 @@ export class SkillsController {
   // Удаление навыка
   @Delete(':id')
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Удалить скилл' })
+  @ApiResponse({ status: 204, description: 'Скилл удален' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteSkill(
     @Param('id', ParseIntPipe) id: number,
@@ -79,6 +105,9 @@ export class SkillsController {
   // Добавление навыка в избранное
   @Post(':id/favorite')
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Добавить скилл в избранное' })
+  @ApiOkResponse({ description: 'Навык добавлен в избранное' })
   @HttpCode(HttpStatus.OK)
   async addSkillToFavorites(
     @Param('id', ParseIntPipe) id: number,
@@ -90,6 +119,9 @@ export class SkillsController {
   // Удаление навыка из избранного
   @Delete(':id/favorite')
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Удалить скилл из избранного' })
+  @ApiResponse({ status: 204, description: 'Скилл удален из избранного' })
   @HttpCode(HttpStatus.NO_CONTENT)
   async removeSkillFromFavorites(
     @Param('id', ParseIntPipe) id: number,
